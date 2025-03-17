44b28c2f6f1c711a0b0fec851be6883ae7cffca3
salonce <miedzyniebem@gmail.com>
2025-03-17 23:05:01 +0100

    added more pictureFactory tests

--- src/test/java/org/example/Flyweight/PictureFactoryTest.java
+++ src/test/java/org/example/Flyweight/PictureFactoryTest.java
@@ -4,8 +4,6 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 class PictureFactoryTest {
 
     private PictureFactory pictureFactory;
@@ -22,10 +20,23 @@ class PictureFactoryTest {
         picture1 = pictureFactory.getPicture("smilingEmoji");
         picture2 = pictureFactory.getPicture("smilingEmoji");
         Assertions.assertSame(picture1, picture2);
+        picture2 = pictureFactory.getPicture("firstAvatar");
+        Assertions.assertNotSame(picture1, picture2);
     }
 
     @Test
-    void otherTest(){
+    void accurateData(){
+        picture1 = pictureFactory.getPicture("smilingEmoji");
+        picture2 = pictureFactory.getPicture("firstAvatar");
+        Assertions.assertNotNull(picture1, "Picture1 shouldn't be null.");
+        Assertions.assertNotNull(picture2, "Picture2 shouldn't be null.");
+        Assertions.assertTrue(picture1 instanceof EmojiPicture, "Picture1 is an instance of emoji picture.");
+        Assertions.assertTrue(picture2 instanceof AvatarPicture, "Picture2 is an instance of avatar picture.");
+    }
 
+    @Test
+    void invalidCallThrowsException(){
+        Exception exception = Assertions.assertThrows(IllegalArgumentException.class, () -> pictureFactory.getPicture("invalidKey"));
+        Assertions.assertEquals("Requested invalid picture key.", exception.getMessage());
     }
 }
